66f4a29ce0a435b6ec2a2dc9dd3527322e96ea00
victochen <victochen@deloitte.com>
2025-06-23 12:56:39 -0500

    Updated the DisplayImage program to test out image read, view, and write; this is not fully tested yet because the sample image library is not working correctly.

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,30 +1,31 @@
-// https://docs.opencv.org/4.11.0/db/df5/tutorial_linux_gcc_cmake.html
+// https://docs.opencv.org/4.11.0/db/deb/tutorial_display_image.html
 
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+ 
+#include <iostream>
  
 using namespace cv;
  
-int main(int argc, char** argv )
+int main()
 {
-    if ( argc != 2 )
+    std::string image_path = samples::findFile("starry_night.jpg");
+    Mat img = imread(image_path, IMREAD_COLOR);
+ 
+    if(img.empty())
     {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
     }
  
-    Mat image;
-    image = imread( argv[1], IMREAD_COLOR );
+    imshow("Display window", img);
+    int k = waitKey(0); // Wait for a keystroke in the window
  
-    if ( !image.data )
+    if(k == 's')
     {
-        printf("No image data \n");
-        return -1;
+        imwrite("starry_night.png", img);
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
- 
-    waitKey(0);
  
     return 0;
 }
