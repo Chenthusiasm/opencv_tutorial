e6d751c03520d658be7bc21c38736fa677eeebca
victochen <victochen@deloitte.com>
2025-06-26 13:36:42 -0500

    Updated the CMakeList.txt file to better match the Modal AI OpenCV projects. Also added a src and include folder like how Modal AI does their OpenCV projects.

---
+++ include/DisplayImage.h
@@ -0,0 +1,10 @@
+#ifndef _DISPLAY_IMAGE_H_
+#define _DISPLAY_IMAGE_H_
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+
+#endif // _DISPLAY_IMAGE_H_
