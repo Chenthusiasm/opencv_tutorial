9a4d33bfb20f0ec2ad3c89a84bb8d36b4e05a39a
victochen <victochen@deloitte.com>
2025-07-09 11:01:22 -0500

    Updated the identify circle to handle a video instead of just an image. Also added some initial code for splitting out the different channels.

--- src/DisplayImage.cpp
+++ src/DisplayImage.cpp
@@ -4,51 +4,84 @@
 
 using namespace cv;
 using namespace std;
+
+String detectedCirclesTitle = "detected circles";
+String grayscaleTitle = "grayscale";
+String blurredTitle = "blurred (median)";
+String redChannelTitle = "red channel";
+
 int main(int argc, char** argv)
 {
-    const char* filename = argc >=2 ? argv[1] : "smarties.png";
-    // Loads an image
-    Mat src = imread( samples::findFile( filename ), IMREAD_COLOR );
-    // Check if image is loaded fine
-    if(src.empty()){
-        printf(" Error opening image\n");
-        printf(" Program Arguments: [image_name -- default %s] \n", filename);
-        return EXIT_FAILURE;
-    }
+  	// show help
+  	if(argc<2){
+    	cout<<
+      		" Usage: DisplayImage <video_name>\n"
+			" examples:\n"
+			" DisplayImage Bolt/img/%04d.jpg\n"
+			" DisplayImage faceocc2.webm\n"
+			<< endl;
+    	return 0;
+	}
+
+    // declare variables
+	Mat frame;
+
+	// set input video
+	std::string video = argv[1];
+	VideoCapture cap(video);
     
-    int rows = src.rows;
-    int cols = src.cols;
-    printf("%s: rows=%d, cols=%d\n", filename, rows, cols);
-    // Size srcSize = src.size();
-    // rows = srcSize.height;
-    // cols = srcSize.width;
-    // printf("%s: height=%d, width=%d\n", filename, rows, cols);
-    Mat gray;
-    cvtColor(src, gray, COLOR_BGR2GRAY);
-    imshow("grayscale", gray);
-    Mat blurred;
-    medianBlur(gray, blurred, 5);
-    imshow("median blurred", blurred);
-    vector<Vec3f> circles;
-    HoughCircles(blurred, circles, HOUGH_GRADIENT, 1,
-                 blurred.rows/16,  // change this value to detect circles with different distances to each other
-                 100, 30, 1, 30 // change the last two parameters
-            // (min_radius & max_radius) to detect larger circles
-    );
-    for( size_t i = 0; i < circles.size(); i++ )
-    {
-        Vec3i c = circles[i];
-        Point center = Point(c[0], c[1]);
-        // circle center
-        circle( src, center, 1, Scalar(0,255,0), 3, LINE_AA);
-        // circle outline
-        int radius = c[2];
-        circle( src, center, radius, Scalar(0,0,255), 3, LINE_AA);
-		// print circle data to terminal
-		printf("[%lu]: (%d, %d) r=%d\n", i, c[0], c[1], c[2]);
+    for ( size_t i; ; ++i ) {
+        cap >> frame;
+
+        // stop the program if there are no more images
+        if ((frame.rows == 0) || (frame.cols == 0)) {
+            break;
+        }
+
+        Mat gray;
+        Mat redChannel;
+        Mat blurred;
+        //vector<Mat> channels(3);
+        //split(frame, channels);
+        // zero out blue channel
+        //channels[0]=Mat::zeros(Size(frame.rows, frame.cols), CV_8UC1);
+        // zero out green channel
+        //channels[1]=Mat::zeros(Size(frame.rows, frame.cols), CV_8UC1);
+        //imshow("blue", channels[0]);
+        //imshow("green", channels[1]);
+        //imshow("red", channels[2]);
+        //merge(channelVector, redChannel);
+        //imshow(redChannelTitle, redChannel);
+
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+        imshow(grayscaleTitle, gray);
+        medianBlur(gray, blurred, 25);
+        imshow(blurredTitle, blurred);
+
+        vector<Vec3f> circles;
+        HoughCircles(blurred, circles, HOUGH_GRADIENT, 1,
+            blurred.rows/10,
+            90, 90/3, 5, 100
+        );
+
+        for(size_t j = 0; j < circles.size(); ++j) {
+            Vec3i c = circles[j];
+            Point center = Point(c[0], c[1]);
+            // circle center
+            circle(frame, center, 1, Scalar(0,255,0), 3, LINE_AA);
+            // circle outline
+            int radius = c[2];
+            circle(frame, center, radius, Scalar(0,0,255), 3, LINE_AA);
+            // print circle data to terminal
+            //printf("[%lu-%lu]: (%d, %d) r=%d\n", i, j, c[0], c[1], c[2]);
+        }
+
+        imshow(detectedCirclesTitle, frame);
+        // quit on ESC button
+        if (waitKey(1) == 27) {
+            break;
+        }
     }
-    
-    imshow("detected circles", src);
-    waitKey();
     return EXIT_SUCCESS;
+    Mat gray;
 }
