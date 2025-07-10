540d0358a8a4985b908f5faddbb4649873ab5151
victochen <victochen@deloitte.com>
2025-07-10 12:01:10 -0500

    Removed the using namespace cv to make it easier to know the OpenCV functions and structures.

--- src/DisplayImage.cpp
+++ src/DisplayImage.cpp
@@ -2,18 +2,18 @@
 
 #include "DisplayImage.h"
 
-using namespace cv;
 using namespace std;
 
-static String const detectedCirclesTitle = "detected circles";
-static String const grayscaleTitle = "grayscale";
-static String const blurredTitle = "blurred (median)";
-static String const redChannelTitle = "red channel";
-static String const subImageTitle = "sub image";
+static string const detectedCirclesTitle = "detected circles";
+static string const grayscaleTitle = "grayscale";
+static string const blurredTitle = "blurred (median)";
+static string const redChannelTitle = "red channel";
+static string const subImageTitle = "sub image";
 
 // note that the K size must be an odd number
 static int const blurKSize = 11;
 static int const subImageBorder = 0;
+
 
 int main(int argc, char** argv)
 {
@@ -29,11 +29,11 @@ int main(int argc, char** argv)
 	}
 
     // declare variables
-	Mat frame;
+	cv::Mat frame;
 
 	// set input video
 	std::string video = argv[1];
-	VideoCapture cap(video);
+	cv::VideoCapture cap(video);
     
     for (size_t i = 0; ; ++i) {
         cap >> frame;
@@ -43,29 +43,29 @@ int main(int argc, char** argv)
             break;
         }
 
-        Mat gray;
+        cv::Mat gray;
         //Mat redChannel;
-        Mat blurred;
+        cv::Mat blurred;
         #if 1
-        vector<Mat> channels(3);
+        vector<cv::Mat> channels(3);
         split(frame, channels);
-        //imshow("red", channels[2]);
-        medianBlur(channels[2], blurred, blurKSize);
+        //cv::imshow("red", channels[2]);
+        cv::medianBlur(channels[2], blurred, blurKSize);
         #else
-        cvtColor(frame, gray, COLOR_BGR2GRAY);
-        //imshow(grayscaleTitle, gray);
-        medianBlur(gray, blurred, blurKSize);
+        cv::cvtColor(frame, gray, COLOR_BGR2GRAY);
+        //cv::imshow(grayscaleTitle, gray);
+        cv::medianBlur(gray, blurred, blurKSize);
         #endif
-        //imshow(blurredTitle, blurred);
+        //cv::imshow(blurredTitle, blurred);
 
-        vector<Vec3f> circles;
-        HoughCircles(blurred, circles, HOUGH_GRADIENT, 1,
+        vector<cv::Vec3f> circles;
+        HoughCircles(blurred, circles, cv::HOUGH_GRADIENT, 1,
             blurred.rows/10,
             90, 90/3, 10, 300
         );
 
         for(size_t j = 0; j < circles.size(); ++j) {
-            Vec3i c = circles[j];
+            cv::Vec3i c = circles[j];
             
             // verify that each circle falls within the color thresholds
             int width = (c[2] + subImageBorder) * 2;
@@ -82,14 +82,14 @@ int main(int argc, char** argv)
                 printf("ERROR: (x, y) = (%d, %d) width = %d, height = %d\n", x, y, width, height);
             }
             // create a cropped sub-image
-            Mat subImage = frame(Rect(x, y, width, height));
+            cv::Mat subImage = frame(cv::Rect(x, y, width, height));
             // invert the image
-            Mat inverseImage = ~subImage;
+            cv::Mat inverseImage = ~subImage;
             // convert image to HSV color
-            Mat inverseHSV;
-            cvtColor(inverseImage, inverseHSV, COLOR_BGR2HSV);
-            Mat mask;
-            inRange(inverseHSV, Scalar(90 - 10, 64, 32), Scalar(90 + 10, 255, 255), mask);
+            cv::Mat inverseHSV;
+            cv::cvtColor(inverseImage, inverseHSV, cv::COLOR_BGR2HSV);
+            cv::Mat mask;
+            inRange(inverseHSV, cv::Scalar(90 - 10, 64, 32), cv::Scalar(90 + 10, 255, 255), mask);
             int count = countNonZero(mask);
             size_t pixels = inverseHSV.total();
             double activePercent = (double)count / (double)pixels;
@@ -98,28 +98,27 @@ int main(int argc, char** argv)
                 continue;
             }
 
-            imshow(subImageTitle, subImage);
+            // imshow(subImageTitle, subImage);
             // if ((activePercent > 0.10) && (activePercent < 0.50)) {
-            //     waitKey(0);
+            //     cv::waitKey(0);
             // }
 
             // circle center
-            Point center = Point(c[0], c[1]);
-            circle(frame, center, 1, Scalar(0,255,0), 3, LINE_AA);
+            cv::Point center = cv::Point(c[0], c[1]);
+            cv::circle(frame, center, 1, cv::Scalar(0,255,0), 3, cv::LINE_AA);
             // circle outline
             int radius = c[2];
-            circle(frame, center, radius, Scalar(255,255,0), 3, LINE_AA);
+            cv::circle(frame, center, radius, cv::Scalar(255,255,0), 3, cv::LINE_AA);
             // print circle data to terminal
             //printf("[%lu-%lu]: (%d, %d) r=%d\n", i, j, c[0], c[1], c[2]);
         }
 
         // show the image
-        imshow(detectedCirclesTitle, frame);
+        cv::imshow(detectedCirclesTitle, frame);
         // quit on ESC button
-        if (waitKey(1) == 27) {
+        if (cv::waitKey(1) == 27) {
             break;
         }
     }
     return EXIT_SUCCESS;
-    Mat gray;
 }
